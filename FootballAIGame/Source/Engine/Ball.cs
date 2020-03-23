991a656c69db354566593733352e467a1ab23287
aaron <aaronwillem@hotmail.com>
2020-03-23 10:52:22 +0100

    initial commit

---
+++ FootballAIGame/Source/Engine/Ball.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+
+namespace FootballAIGame
+{
+    class Ball
+    {
+        private Vector2 pos;
+        private Vector2 dims;
+        public Texture2D mySprite;
+
+        public Ball()
+        {
+            pos = new Vector2(9, 9);
+            dims = new Vector2(9, 9);
+            mySprite = Globals.content.Load<Texture2D>("2d/sprite");
+        }
+
+        public void Update() {
+
+        }
+
+        public void Draw() {
+
+        }
+    }
+}
---
+++ FootballAIGame/Source/Engine/FootballPlayer.cs
@@ -0,0 +1,78 @@
+﻿#region Includes
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+#endregion
+
+namespace FootballAIGame
+{
+    class FootballPlayer
+    {
+        public Vector2 pos;
+        public Vector2 dims;
+        public string name;
+        public int speed;
+        public int strength;
+        public int price;
+        public Texture2D mySprite;
+        private delegate void Del(FootballPlayer player);
+        private Del delegateMovement;
+        public McKeyboard key;
+
+        public FootballPlayer(Vector2 pos, Vector2 dims, string name, int speed,
+            int strength, int price, string path, string playerType) {
+            this.pos = pos;
+            this.dims = dims;
+            this.name = name;
+            this.speed = speed;
+            this.strength = strength;
+            this.price = price;
+            mySprite = Globals.content.Load<Texture2D>(path);
+            SetDeligate(playerType);
+        }
+
+        private void SetDeligate(string playerType) {
+            switch (playerType) {
+                case "human":
+                    delegateMovement = FootballTypes.HumanPlayer;
+                    key = new McKeyboard();
+                    break;
+                case "keeper":
+                    delegateMovement = FootballTypes.KeeperAI;
+                    break;
+                case "defender":
+                    delegateMovement = FootballTypes.DefenderAI;
+                    break;
+                case "midfielder":
+                    delegateMovement = FootballTypes.MidfielderAI;
+                    break;
+                case "attacker":
+                    delegateMovement = FootballTypes.AttackerAI;
+                    break;
+            }
+        }
+
+        public void Update() { 
+            delegateMovement(this);
+        }
+
+        public void Draw() {
+            if (mySprite != null) {
+                //these 3 lines are 1 line
+                Globals.spriteBatch.Draw(mySprite, new Rectangle((int)dims.X, (int)dims.Y, (int)pos.X, (int)pos.Y),
+                    null, Color.White, 0.0f, new Vector2(mySprite.Bounds.Width / 2, mySprite.Bounds.Height / 2),
+                    new SpriteEffects(), 0);
+            }
+        }
+    }
+}
